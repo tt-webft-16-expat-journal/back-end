98ac7f5bc475bf74873579850757ee0c284d072f
MorganWilliamson <mwilliamson7695@gmail.com>
2021-01-04 17:34:24 -0600

    User By ID and Posts By User endpoints built and tested in Postman.

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -5,6 +5,7 @@ module.exports = {
     get,
     getBy,
     getById,
+    getPostsByUser,
 };
 
 function get() {
@@ -22,4 +23,8 @@ function getById(id) {
 async function add(user) {
     const [id] = await db("users").insert(user, "id");
     return getById(id);
+}
+
+function getPostsByUser(id) {
+    return db("posts").where({ "posts.user_id": id });
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,7 @@ const Users = require("./users-model");
 const restricted = require("../../config/middleware/restricted");
 
 // USER ENDPOINTS //
+// Return a list of all users.
 router.get("/", restricted, (req, res) => {
     Users.get()
         .then((users) => {
@@ -13,5 +14,31 @@ router.get("/", restricted, (req, res) => {
         });
 });
 
+// Return a single user for a given ID.
+router.get("/:id", restricted, (req, res) => {
+    const id = req.params.id;
+    Users.getById(id)
+        .then((user) => {
+            const { username } = user;
+            res.status(200).json({ username, id });
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
+// Return all posts by a given user.
+router.get("/:id/posts", restricted, (req, res) => {
+    const id = req.params.id;
+
+    // if(!posts), then return "No posts by user."
+    Users.getPostsByUser(id)
+        .then((posts) => {
+            res.status(200).json({ posts });
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+});
 
 module.exports = router;
