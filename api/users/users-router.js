4c0195cac9ec48791fc0789b5acc9fa7f228e9ad
MorganWilliamson <mwilliamson7695@gmail.com>
2021-01-04 17:22:07 -0600

    GET all users written and tested.

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,3 +1,17 @@
 const router = require("express").Router();
+const Users = require("./users-model");
+const restricted = require("../../config/middleware/restricted");
+
+// USER ENDPOINTS //
+router.get("/", restricted, (req, res) => {
+    Users.get()
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
 
 module.exports = router;
